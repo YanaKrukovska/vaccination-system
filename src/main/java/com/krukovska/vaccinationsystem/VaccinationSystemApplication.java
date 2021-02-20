910c91b86fc587093789ec8b9e80f3ba2137ea37
YanaKrukovska <jana.krua@gmail.com>
2021-02-20 16:11:55 +0200

    Initial commit

---
+++ src/main/java/com/krukovska/vaccinationsystem/VaccinationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.krukovska.vaccinationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaccinationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krukovska/vaccinationsystem/VaccinationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krukovska.vaccinationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
